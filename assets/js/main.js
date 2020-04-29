40d3eeb2f90aaaf2e42299a8253a256e44d54d5f
Thomas Corbin <thomas.corbin22@gmail.com>
2020-04-29 19:07:29 +0800

    Reorganise Folders

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+    $(".icon-map").mouseover(function () {
+        changeHover(this, "assets/images/contact-me/location-hover-01.png")
+    }).mouseout(function () {
+        changeHover(this, "assets/images/contact-me/location-01.png")
+    });
+    $(".icon-phone").mouseover(function () {
+        changeHover(this, "assets/images/contact-me/phone-hover-01.png")
+    }).mouseout(function () {
+        changeHover(this, "assets/images/contact-me/phone-01.png")
+    });
+    $(".icon-envelope").mouseover(function () {
+        changeHover(this, "assets/images/contact-me/email-hover-01.png")
+    }).mouseout(function () {
+        changeHover(this, "assets/images/contact-me/email-01.png")
+    });
+})
+
+window.onload = function () {
+    changeOpacity(".carousel-overlay-initial", 0.5);
+    changeOpacity("h1", 1);
+    changeOpacity("header > div > p", 1);
+}
+
+navSlide();
+setDivHeight();
+splatterTimeOpen()
+splatterTimeClose()
+
+window.addEventListener('scroll', scrollAppearWelcome);
+window.addEventListener('scroll', scrollAppearCoding);
+window.addEventListener('scroll', scrollSlideWelcome);
+window.addEventListener('scroll', scrollSlideHobbies);
+window.addEventListener('scroll', scrollSlideArchitectureLarge);
+window.addEventListener('scroll', scrollSlideArchitectureSmall);
+window.addEventListener('scroll', scrollSlideHoriontalLine);
+window.addEventListener('scroll', scrollGrowHobbies);
+window.addEventListener('resize', setDivHeight);
+window.addEventListener('focus', setDivHeight);
+window.addEventListener('scroll', setLineDimensions);
+window.addEventListener('resize', setLineDimensions);
+window.addEventListener('scroll', animationOnScrollBlog);
+// window.addEventListener('scroll', animationOnScrollContact);
+window.addEventListener("DOMContentLoaded", function () {
+
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+        form.reset();
+        button.style = "display: none ";
+        status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+clickChange(architectureTitle1, architectureSubTitle1, architecturePara1a, architecturePara1b, "architecture-img-1");
+clickChange(architectureTitle2, architectureSubTitle2, architecturePara2a, architecturePara2b, "architecture-img-2");
+clickChange(architectureTitle3, architectureSubTitle3, architecturePara3a, architecturePara3b, "architecture-img-3");
+clickChange(architectureTitle4, architectureSubTitle4, architecturePara4a, architecturePara4b, "architecture-img-4");
+clickChange(architectureTitle5, architectureSubTitle5, architecturePara5a, architecturePara5b, "architecture-img-5");
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 300
+});
+
+AOS.init();
---
+++ assets/js/variables.js
@@ -0,0 +1,25 @@
+// Variables used with clickChange
+
+const architectureTitle1 = "Sentosa-Brani Masterplan"
+const architectureTitle2 = "Shenzhen Bay Avenue East Extension"
+const architectureTitle3 = "HKIA Sky Bridge"
+const architectureTitle4 = "Healex House: Phase 2, Esher"
+const architectureTitle5 = "New Road, Esher"
+
+const architectureSubTitle1 = "WilkinsonEyre"
+const architectureSubTitle2 = "WilkinsonEyre"
+const architectureSubTitle3 = "WilkinsonEyre"
+const architectureSubTitle4 = "Clive Chapman Architects"
+const architectureSubTitle5 = "Clive Chapman Architects"
+
+const architecturePara1a = "During my tenure in WilkinsonEyre, I worked alongside Grant Associates and a host of other interdisciplinary practices to develop the architectural design for the upcoming Sentosa-Brani masterplan."
+const architecturePara2a = "I was part of the WilkinsonEyre team that produced the final submission for Shenzhen Bay Avenue, which ended up winning the major international design submission for the urban design of the Greater Bay Area. "
+const architecturePara3a = "I worked on developing the internal and external aesthetics of the Sky Bridge at the Hong Kong International Airport (HKIA), which is anticipated to be the world’s longest airside bridge."
+const architecturePara4a = "I worked on the planning documentation for the major refurbishment and extension of the existing Healix International HQ in order to accommodate their recent acceleration of growth."
+const architecturePara5a = "Currently an underutilised amenity space comprising a dilapidated tennis court and garage, the proposals that I worked on seek to provide a four-bedroom house combined with associated parking and the beautification of the landscape."
+
+const architecturePara1b = "The masterplan will roll out over the next three decades and will split the island into five distinct character zones: Vibrant Cluster, Island Heart, Waterfront, Ridgeline and Beachfront."
+const architecturePara2b = "The proposals call for a 1km multi-layered pedestrian route that acts engages the public realm with healthy and sustainable living, whilst seamlessly connecting the city to the waterfront."
+const architecturePara3b = "Currently the main structure of the Sky Bridge was successfully lifted into its final position and directly connects Terminal 1 with the North Satellite Concourse."
+const architecturePara4b = "The proposals will incorporate over 1500m2 of office space over four floors whilst including plenty of retail usage at the ground floor."
+const architecturePara5b = "A key goal for the new dwelling was to minimise the impact on the surrounding neighbourhood properties in terms of both scale and siting."
